cb674694b6e3c1c37bd2af151efcab20f510fdae
Alex Lukyanau <cybergomel@gmail.com>
2015-08-31 16:45:39 -0400

    Css Minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,41 @@
-/*
- * @author Alex Lukyanau
- * @copyright Sashas IT Support Inc
- * 
- * */
+ 
+//grunt.option('target')
+
 module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
+    theme: grunt.option('theme'),
+    
+    copy: {
+      files: {    	
+    	nonull: true,
+    	expand: true,
+    	cwd: '../skin/frontend/<%= theme %>/css/',
+    	src: ["*.*", "**/*.*"],
+        dest: '../skin/frontend/<%= theme %>/source/css/'
+      }  
+    },
+    
+    cssmin: {
+    	files: {
+            expand: true,
+            cwd: '../skin/frontend/<%= theme %>/css/',
+            src: ["*.*", "**/*.*"],
+            dest: '../skin/frontend/<%= theme %>/min/css/'             
+        }
+    },          
+    
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
+  grunt.loadNpmTasks('grunt-yui-compressor');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  //grunt.loadNpmTasks('grunt-contrib-cssmin');
+  
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['copy','cssmin']);
 
 };
